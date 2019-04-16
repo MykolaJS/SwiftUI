b8cb09b3e311f24a6f4823bbf49612566f8f9804
Mykola Vasylkiv <mvasilkiv21@gmail.com>
2019-04-17 00:22:29 +0300

    add litle bit styles

--- loginPage/ViewController.swift
+++ loginPage/ViewController.swift
@@ -39,8 +39,8 @@ class ViewController: UIViewController {
     
     let haveAccountButton: UIButton = {
         let haveAccount = UIButton(type: .system)
-            haveAccount.backgroundColor = .yellow
-        let attributeTitle = NSAttributedString(string: "Don't have an account", attributes: [NSAttributedString.Key.backgroundColor: UIColor.white])
+            haveAccount.backgroundColor = GREEN_THEME
+        let attributeTitle = NSAttributedString(string: "Don't have an account", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         haveAccount.setAttributedTitle(attributeTitle, for: .normal)
         return haveAccount
     }()
@@ -55,6 +55,10 @@ class ViewController: UIViewController {
         setupHavaAccoutnButton()
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     func setupTextFieldComponents() {
         setupEmailTextFiled()
         setupPasswordTextFiled()
@@ -64,10 +68,10 @@ class ViewController: UIViewController {
         view.addSubview(emailTextField)
         
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
-        emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
         emailTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
         emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
         emailTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        emailTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
     }
     
